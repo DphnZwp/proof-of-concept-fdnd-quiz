26e686211d85c026889c4c14aeb988a826d28895
DphnZwp <zwuupdaphne@gmail.com>
2022-06-17 22:40:13 +0200

    quiz design

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,11 +8,16 @@ const answeredEl = document.querySelector(".score-board .answered-number")
 let question, answer
 let options = []
 let score = 0
-let answeredQus = 0
+let answeredQuestion = 0
 
-window.addEventListener("DOMContentLoaded", () => {
-  quizApp()
-});
+quizApp()
+
+async function fetchQuiz(){
+  const response = await fetch(baseURL)
+  const data = await response.json()
+  // console.log(data.results)
+  return data.results
+}
 
 async function quizApp() {
   const data = await fetchQuiz()
@@ -24,7 +29,7 @@ async function quizApp() {
   options.splice(Math.floor(Math.random() * options.length + 1), 0, answer)
   // console.log(options, answer)
   console.log(answer)
-  generateTemplate(question, options)
+  renderQuiz(question, options)
 }
 
 form.addEventListener('submit', (e) => {
@@ -32,20 +37,14 @@ form.addEventListener('submit', (e) => {
 
   if(e.target.quiz.value){
     checkQuiz(e.target.quiz.value)
+    e.target.querySelector('button').style.display = 'none'
+    // generateFinishButton()
   }else{
     return
   }
 })
 
-async function fetchQuiz(){
-  const response = await fetch(baseURL)
-  const data = await response.json()
-
-  // console.log(data.results)
-  return data.results
-}
-
-function generateTemplate(question, options) {
+function renderQuiz(question, options) {
   optionsEl.innerHTML = "";
   qusEl.innerText = question
   options.map((option, index) => {
@@ -53,21 +52,34 @@ function generateTemplate(question, options) {
     item.classList.add("option")
     item.innerHTML = `
     <input type="radio" id="option${index + 1}" value="${option}" name="quiz">
-    <label class="option-label" for="option${index + 1}">${index + 1} ${option}</label>
+    <label for="option${index + 1}">
+      <span class="letter">${String.fromCharCode(96 + index + 1).toUpperCase()}</span> ${option}
+    </label>
   `
     optionsEl.appendChild(item)
   })
 }
 
 function checkQuiz (selected){
-  answeredQus++
+  answeredQuestion++
   if(selected === answer){
     score++
   }
   updateScoreBoard()
+  form.quiz.forEach(input => {
+    if(input.value === answer){
+      input.parentElement.classList.add('correct')
+    }
+  })
 }
 
 function updateScoreBoard(){
   scoreEl.innerText = score
-  answeredEl.innerText = answeredQus
+  answeredEl.innerText = answeredQuestion
 }
+
+// function generateFinishButton() {
+//   const finishButton = document.createElement('button')
+//   finishButton.innerText = 'Finish'
+//   finishButton.setAttribute('')
+// }
