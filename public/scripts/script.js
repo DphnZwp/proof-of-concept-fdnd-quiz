2ec8b49da11a07073cde4ea8a1b1b65e53b09505
DphnZwp <zwuupdaphne@gmail.com>
2022-06-21 13:58:46 +0200

    next button not working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,78 +1,130 @@
-const baseURL = "https://opentdb.com/api.php?amount=1&type=multiple"
+const apiBase = '/json/quiz.json'
 const form = document.querySelector(".quiz-form")
 const qusEl = document.querySelector(".question")
 const optionsEl = document.querySelector(".all-options")
 const scoreEl = document.querySelector(".score-board .score-number")
 const answeredEl = document.querySelector(".score-board .answered-number")
+const nextButton = document.querySelector(".nextButton")
 
 let question, answer
 let options = []
 let score = 0
 let answeredQuestion = 0
 
-quizApp()
+// renderQuiz()
+getCurrentIndex(1)
 
 async function fetchQuiz() {
-  const response = await fetch(baseURL)
+  const response = await fetch(apiBase)
   const data = await response.json()
-  // console.log(data.results)
-  return data.results
+  options = data
 }
 
-async function quizApp() {
-  const data = await fetchQuiz()
-  question = data[0].question
-  options = []
-  answer = data[0].correct_answer
-  data[0].incorrect_answers.map(option => options.push(option))
-  // Place correct answer in a random place between the incorrect options
-  options.splice(Math.floor(Math.random() * options.length + 1), 0, answer)
-  // console.log(options, answer)
-  console.log(answer)
-  renderQuiz(question, options)
+// async function renderQuiz() {
+//   await fetchQuiz()
+//   // console.log(options)
+//   let questions = ""
+//   let answers = ""
+//   options.forEach(quiz => {
+//     rightAnswer = quiz.correct_answer
+//     let possibleAnswers = quiz.incorrect_answers
+//     possibleAnswers.splice(Math.floor(Math.random() * 3), 0, rightAnswer)
+//     possibleAnswers.map((answer, index) => {
+//       answers += `
+//         <div class="option">
+//           <input type="radio" id="option${index + 1}" value="${answer}" name="quiz">
+//           <label for="option${index + 1}">
+//             <span class="letter">${String.fromCharCode(96 + index + 1).toUpperCase()}</span> ${answer}
+//           </label>
+//         </div>
+//       `
+//     })
+//     questions += `
+//       <h1> ${quiz.question}</h1>
+//     `
+//   })
+//   qusEl.innerHTML = questions
+//   optionsEl.innerHTML = answers
+// }
+
+async function getCurrentIndex(questionId) {
+  await fetchQuiz()
+  let currentIndex = options.findIndex(question => question.questionId === questionId)
+  let nextIndex = currentIndex+1 > options.length ? 0 : currentIndex+1
+  let currentQuestion = options[currentIndex]
+  let nextQuestion = options[nextIndex]
+  renderQuestion(currentQuestion)
+  nextPage(currentQuestion, nextQuestion)
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  if(e.target.quiz.value) {
-    checkQuiz(e.target.quiz.value)
-    e.target.querySelector('button').style.display = 'none'
-  } else {
-    return
-  }
-})
-
-function renderQuiz(question, options) {
-  optionsEl.innerHTML = "";
-  qusEl.innerText = question
-  options.map((option, index) => {
+function renderQuestion(currentQuestion) {
+  rightAnswer = currentQuestion.correct_answer
+  let possibleAnswers = currentQuestion.incorrect_answers
+  possibleAnswers.splice(Math.floor(Math.random() * 3), 0, rightAnswer)
+  qusEl.innerText = currentQuestion.question
+  possibleAnswers.map((answer, index) => {
     const item = document.createElement("div")
     item.classList.add("option")
     item.innerHTML = `
-    <input type="radio" id="option${index + 1}" value="${option}" name="quiz">
+    <input type="radio" id="option${index + 1}" value="${answer}" name="quiz">
     <label for="option${index + 1}">
-      <span class="letter">${String.fromCharCode(96 + index + 1).toUpperCase()}</span> ${option}
+      <span class="letter">${String.fromCharCode(96 + index + 1).toUpperCase()}</span> ${answer}
     </label>
   `
     optionsEl.appendChild(item)
   })
 }
 
-function checkQuiz (selected) {
-  answeredQuestion++
-  if(selected === answer) {
-    score++
-  }
-  updateScoreBoard()
-  form.quiz.forEach(input => {
-    if(input.value === answer) {
-      input.parentElement.classList.add('correct')
-    }
+function renderNextQuestion(nextQuestion) {
+  rightAnswer = nextQuestion.correct_answer
+  let possibleAnswers = nextQuestion.incorrect_answers
+  possibleAnswers.splice(Math.floor(Math.random() * 3), 0, rightAnswer)
+  qusEl.innerText = nextQuestion.question
+  possibleAnswers.map((answer, index) => {
+    const item = document.createElement("div")
+    item.classList.add("option")
+    item.innerHTML = `
+    <input type="radio" id="option${index + 1}" value="${answer}" name="quiz">
+    <label for="option${index + 1}">
+      <span class="letter">${String.fromCharCode(96 + index + 1).toUpperCase()}</span> ${answer}
+    </label>
+  `
+    optionsEl.appendChild(item)
   })
 }
 
-function updateScoreBoard() {
-  scoreEl.innerText = score
-  answeredEl.innerText = answeredQuestion
+function nextPage(currentQuestion, nextQuestion) {
+  nextButton.addEventListener('click', () => {
+    renderNextQuestion(nextQuestion)
+  })
 }
+
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault()
+
+//   if(e.target.quiz.value) {
+//     checkQuiz(e.target.quiz.value)
+//   } else {
+//     return
+//   }
+// })
+
+// function checkQuiz (selected) {
+//   answeredQuestion++
+//   if(selected === rightAnswer) {
+//     score++
+//   }
+  
+//   updateScoreBoard()
+//   form.quiz.forEach(input => {
+//     if(input.value === rightAnswer) {
+//       input.parentElement.classList.add('correct')
+//     }
+//   })
+// }
+
+// function updateScoreBoard() {
+//   scoreEl.innerText = score
+//   answeredEl.innerText = answeredQuestion
+// }
