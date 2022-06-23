03bef18dda43a0fed67cb5419b3afba54cb7be52
DphnZwp <zwuupdaphne@gmail.com>
2022-06-23 11:16:11 +0200

    quiz to server-side

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,130 +1,56 @@
-const apiBase = '/json/quiz.json'
+const apiBase = 'https://quiz.api.fdnd.nl/v1/question'
 const form = document.querySelector(".quiz-form")
-const qusEl = document.querySelector(".question")
-const optionsEl = document.querySelector(".all-options")
+// const qusEl = document.querySelector(".question")
+// const qusCon = document.querySelector(".question-container")
+// const optionsEl = document.querySelector(".all-options")
 const scoreEl = document.querySelector(".score-board .score-number")
 const answeredEl = document.querySelector(".score-board .answered-number")
-const nextButton = document.querySelector(".nextButton")
+// const nextButton = document.querySelector(".nextButton")
 
-let question, answer
-let options = []
 let score = 0
 let answeredQuestion = 0
-
-// renderQuiz()
-getCurrentIndex(1)
 
 async function fetchQuiz() {
   const response = await fetch(apiBase)
   const data = await response.json()
-  options = data
+  options = data.data
 }
 
-// async function renderQuiz() {
-//   await fetchQuiz()
-//   // console.log(options)
-//   let questions = ""
-//   let answers = ""
-//   options.forEach(quiz => {
-//     rightAnswer = quiz.correct_answer
-//     let possibleAnswers = quiz.incorrect_answers
-//     possibleAnswers.splice(Math.floor(Math.random() * 3), 0, rightAnswer)
-//     possibleAnswers.map((answer, index) => {
-//       answers += `
-//         <div class="option">
-//           <input type="radio" id="option${index + 1}" value="${answer}" name="quiz">
-//           <label for="option${index + 1}">
-//             <span class="letter">${String.fromCharCode(96 + index + 1).toUpperCase()}</span> ${answer}
-//           </label>
-//         </div>
-//       `
-//     })
-//     questions += `
-//       <h1> ${quiz.question}</h1>
-//     `
+// function nextQuestion() {
+//   nextButton.addEventListener('click', () => {
+//     // Change currentIndex into nextIndex
+//     console.log('Ga naar de volgende vraag en antwoorden')
 //   })
-//   qusEl.innerHTML = questions
-//   optionsEl.innerHTML = answers
 // }
 
-async function getCurrentIndex(questionId) {
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  if(e.target.quiz.value) {
+    checkQuiz(e.target.quiz.value)
+  } else {
+    return
+  }
+})
+
+async function checkQuiz (selected) {
   await fetchQuiz()
-  let currentIndex = options.findIndex(question => question.questionId === questionId)
-  let nextIndex = currentIndex+1 > options.length ? 0 : currentIndex+1
-  let currentQuestion = options[currentIndex]
-  let nextQuestion = options[nextIndex]
-  renderQuestion(currentQuestion)
-  nextPage(currentQuestion, nextQuestion)
-}
+  rightAnswer = options.correct_answer
 
-function renderQuestion(currentQuestion) {
-  rightAnswer = currentQuestion.correct_answer
-  let possibleAnswers = currentQuestion.incorrect_answers
-  possibleAnswers.splice(Math.floor(Math.random() * 3), 0, rightAnswer)
-  qusEl.innerText = currentQuestion.question
-  possibleAnswers.map((answer, index) => {
-    const item = document.createElement("div")
-    item.classList.add("option")
-    item.innerHTML = `
-    <input type="radio" id="option${index + 1}" value="${answer}" name="quiz">
-    <label for="option${index + 1}">
-      <span class="letter">${String.fromCharCode(96 + index + 1).toUpperCase()}</span> ${answer}
-    </label>
-  `
-    optionsEl.appendChild(item)
-  })
-}
-
-function renderNextQuestion(nextQuestion) {
-  rightAnswer = nextQuestion.correct_answer
-  let possibleAnswers = nextQuestion.incorrect_answers
-  possibleAnswers.splice(Math.floor(Math.random() * 3), 0, rightAnswer)
-  qusEl.innerText = nextQuestion.question
-  possibleAnswers.map((answer, index) => {
-    const item = document.createElement("div")
-    item.classList.add("option")
-    item.innerHTML = `
-    <input type="radio" id="option${index + 1}" value="${answer}" name="quiz">
-    <label for="option${index + 1}">
-      <span class="letter">${String.fromCharCode(96 + index + 1).toUpperCase()}</span> ${answer}
-    </label>
-  `
-    optionsEl.appendChild(item)
-  })
-}
-
-function nextPage(currentQuestion, nextQuestion) {
-  nextButton.addEventListener('click', () => {
-    renderNextQuestion(nextQuestion)
-  })
-}
-
-
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault()
-
-//   if(e.target.quiz.value) {
-//     checkQuiz(e.target.quiz.value)
-//   } else {
-//     return
-//   }
-// })
-
-// function checkQuiz (selected) {
-//   answeredQuestion++
-//   if(selected === rightAnswer) {
-//     score++
-//   }
+  answeredQuestion++
+  if(selected === rightAnswer) {
+    score++
+  }
   
-//   updateScoreBoard()
-//   form.quiz.forEach(input => {
-//     if(input.value === rightAnswer) {
-//       input.parentElement.classList.add('correct')
-//     }
-//   })
-// }
+  updateScoreBoard()
+  form.quiz.forEach(input => {
+    if(input.value === rightAnswer) {
+      input.parentElement.classList.add('correct')
+    }
+  })
+}
 
-// function updateScoreBoard() {
-//   scoreEl.innerText = score
-//   answeredEl.innerText = answeredQuestion
-// }
+function updateScoreBoard() {
+  scoreEl.innerText = score
+  answeredEl.innerText = answeredQuestion
+}
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path')
 const compression = require('compression')
 const app = express()
 const PORT = process.env.PORT || 9000
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+const url = 'https://quiz.api.fdnd.nl/v1/question'
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({extended:false})
 
 app.set('views', path.join(__dirname + '/views'))
 app.set('view engine', 'ejs')
@@ -13,14 +17,33 @@ app.get('/', (request, response) => {
   response.render('index')
 })
 
+// POST form
+// app.post('/quiz-aanmaken', urlencodedParser, (request,response) =>{
+//   const postData = {
+//     method: 'post',
+//     body: JSON.stringify(request.body),
+//     headers: {'Content-Type': 'application/json'}
+//   }
+//   fetchJson(url, postData).then(function () {
+//     response.render('quiz-aanmaken')
+//   })
+// })
+
+// app.get('/quiz-aanmaken', (request, response) => {
+//     response.render('quiz-aanmaken')
+// })
+
 app.get('/quiz', (request, response) => {
-  response.render('quiz')
+  fetchJson(url).then(function (
+    jsonData
+  ) {
+    response.render('quiz', {
+      questions: jsonData.data,
+      rightAnswer: jsonData.data[0].correct,
+      possibleAnswers: jsonData.data[0].incorrect_answer,
+    })
+  })
 })
-
-app.get('/highscores', (request, response) => {
-  response.render('highscores')
-})
-
 
 // Cache Headers
 app.use((req, res, next) => {
@@ -35,3 +58,10 @@ app.use(compression())
 const server = app.listen(PORT, () => {
   console.log(`Application started on port: ${PORT}`)
 })
+
+// Fetch
+async function fetchJson(url, postData = {}) {
+  return await fetch(url, postData)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
